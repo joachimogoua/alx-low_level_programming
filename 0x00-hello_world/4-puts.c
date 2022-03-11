d9be0d77c17b4c0f821fa26fe3b74fcf3e9e4d27
joachimogoua <ogouadjedjijoachim@gmail.com>
2022-03-10 16:24:59 -0800

    4.hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
-/** 
- *main displays the following sentence " programming is like buildigns a multilingual puzzle
- *return o if it runs good 
- */
+/**
+ * main - displays sentence programming is like buildigns a multilingual puzzle
+ * return:o if it runs good 
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
