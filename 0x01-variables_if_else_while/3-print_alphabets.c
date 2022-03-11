2c52e54d888c7ea51510d3233736f3cd12cadc60
joachimogoua <ogouadjedjijoachim@gmail.com>
2022-03-11 12:11:01 -0800

    4.print alphabgt

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,14 @@
  */
 int main(void)
 {
-	char ch = 'a';
+        char ch = 'a';
 
-	while (ch <= 'z')
-	{
-		if (ch != 'e' && ch != 'q')
-		{
-			putchar(ch);
-		}
-		ch++;
-	}
-	putchar('\n');
-	return (0);
+        while (ch <= 'z')
+        {
+                if (ch != 'e' && ch != 'q')
+                {
+                        putchar(ch);
+                }
+                ch++;
+        }
+        putchar('\n');
