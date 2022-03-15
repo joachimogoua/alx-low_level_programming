1222c4f649a25ee373060581652d0cec84e63326
joachimogoua <ogouadjedjijoachim@gmail.com>
2022-03-15 14:13:57 -0700

    task1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,10 +4,10 @@
  */
 void print_alphabet(void)
 {
-  char ch;
-    for (ch='a' ; ch<='z' ; ch++)
-      {
-	_putchar('c');
-      }
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+	_putchar(ch);
+}
 	_putchar('\n');
 }
