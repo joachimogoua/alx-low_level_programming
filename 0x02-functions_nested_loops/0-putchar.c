ee66fa8556fd76f4a117028a3a03e1227a613636
joachimogoua <ogouadjedjijoachim@gmail.com>
2022-03-15 11:18:07 -0700

    0.puthar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h>"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+for exercice on 02 c 
