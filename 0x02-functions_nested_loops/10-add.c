bedf64e46e03a362c5276b39581c08c7fd1c9d5c
joachimogoua <ogouadjedjijoachim@gmail.com>
2022-03-16 04:21:38 -0700

    task10 adding two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - added two integers.
+ * Return : always 0 it runs good
+ */
+
+int add(int ,int)
+{
+  int a , b ;
+  return (a+b);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,3 +9,4 @@ int _putchar(char c);
 int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
+int add(int,int);
