bf735b26329f48e6a9690792a8be5680999d3e35
joachimogoua <ogouadjedjijoachim@gmail.com>
2022-03-16 04:47:23 -0700

    task10 adding two inteers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,13 @@
 #include "main.h"
 /**
  * add - added two integers.
- * Return : always 0 it runs good
+ * Return : 0 alwaysit runs good
+ * @a adding to b
+ * @b adding to a
+ * return  : result of adding of a+b
  */
 
 int add(int a, int b)
 {
-  return(a+b);
+return (a + b);
 }
