5fa2eb7abb236a9c66e68d44802bfb6bf3da67c3
joachimogoua <ogouadjedjijoachim@gmail.com>
2022-03-16 07:59:41 -0700

    task 11 print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -15,15 +15,18 @@ void print_to_98(int n)
     {
       for (i = n ; i < 98 ; i++)
 	{
-	  printf("i, ");
+	  printf("%d, ",i);
 	}
+      printf('\n');
     }
 else
     {
       for (i = n ; i >= 98 ; i--)
 	{
-	   printf("i, ");
+	    printf("%d, ",i);
 	}
+       printf('\n');
+
     }
-  return (0);
+ 
 }
