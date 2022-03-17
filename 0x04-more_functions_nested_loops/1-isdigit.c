4fac8d060aa06ccd11e1045b80f4c9d3d25e3906
joachimogoua <ogouadjedjijoachim@gmail.com>
2022-03-17 14:05:53 -0700

    1.isdigit check

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _isdigit - check if is digit 
- * Return : 1 if digit 0 else
- * @c: int to be checked 
+ *_isdigit -check if is digit
+ * Return: 1 if digit 0 else
+ * @c: int to be checked
  */
 
 
 int _isdigit(int c)
 {
-  return( c >= '0' && c <= '0');
+return (c >= '0' && c <= '0');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,4 @@
-#ifndef FILE_HOLBERTON
+#ifndef FILE_MAIN
 #define FILE_MAIN
 
 int _putchar(char c);
