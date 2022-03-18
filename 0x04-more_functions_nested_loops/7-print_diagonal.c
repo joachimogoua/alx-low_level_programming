f3d1d787ca8b719c3d83cf9ce4fd89f11fe06706
joachimogoua <ogouadjedjijoachim@gmail.com>
2022-03-18 07:45:30 -0700

    7.print digonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_diagonal -print in diagonal \
+ * @n: number of times
+ */
+
+void print_diagonal(int n)
+{
+while (n > 0)
+{
+_putchar(\ + '0');
+}
+_putcahr('\n');
+}
