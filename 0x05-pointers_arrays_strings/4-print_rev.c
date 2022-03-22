5b8215b19b8b5510605eac47b1ce3a085e16be5c
joachimogoua <ogouadjedjijoachim@gmail.com>
2022-03-22 15:42:26 -0700

    4.print in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - check the code
+ * @s: chaine de caractere
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+int i;
+for (i = strlen(s); i = 0 ; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
