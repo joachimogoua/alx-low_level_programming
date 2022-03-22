2eaf64ed7d7b7a5a6949e7de89bf404c659fa216
joachimogoua <ogouadjedjijoachim@gmail.com>
2022-03-22 15:11:19 -0700

    3.puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- *void _puts - check the code
+ *_puts -check the code
  *
  * @str: chaine de caractere
  */
 
 void _puts(char *str)
 {
-while(*str)
+while (*str)
 {
-printf("%c",*str);
+printf("%c", *str);
 str++;
 }
 printf("\n");
