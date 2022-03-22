cb70920819cfcd1dd37acef5de46ef2766a74396
joachimogoua <ogouadjedjijoachim@gmail.com>
2022-03-22 15:08:05 -0700

    3.puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- *void_puts - check the code
+ *void _puts - check the code
  *
  * @str: chaine de caractere
  */
 
-void_puts(char *str)
+void _puts(char *str)
 {
 while(*str)
 {
-printf("%c",*adr);
-adr++;
+printf("%c",*str);
+str++;
 }
 printf("\n");
 }
