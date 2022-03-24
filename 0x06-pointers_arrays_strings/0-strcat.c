9f89c88ffe3331c3d2840840334d7e3c45de6a20
joachimogoua <ogouadjedjijoachim@gmail.com>
2022-03-24 06:19:39 -0700

    0.strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,23 +7,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
-int len1, len2, i, j;
-len1 = 0, len2 = 0;
-while (src[len1] != '\0')
-{
-len1++;
-}
-while (dest[len2] != '\0')
-{
-len2++;
-}
-j = 0;
-for (i = len1; i <= (len1 + len2); j <= len1)
-{
-dest[i] = src[j];
-j++;
-i++;
-}
-dest[i] = '\0';
-return (dest);
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
 }
