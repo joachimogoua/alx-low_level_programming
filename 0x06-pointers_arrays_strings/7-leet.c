1a74403840a6086f64e6cc685540ed00d8c5b81b
joachimogoua <ogouadjedjijoachim@gmail.com>
2022-03-24 22:32:27 -0700

    7.leet mozart

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,21 +8,21 @@
  */
 char *leet(char *s)
 {
-  int i, j;
+int i, j;
 
-  char *a = "aAeEoOtTlL";
-  char *b = "4433007711";
+char *a = "aAeEoOtTlL";
+char *b = "4433007711";
 
-  for (i = 0; s[i] != '\0'; i++)
-    {
-      for (j = 0; j < 10; j++)
-	{
-	  if (s[i] == a[j])
-	    {
-	      s[i] = b[j];
-	    }
-	}
-    }
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; j < 10; j++)
+{
+if (s[i] == a[j])
+{
+s[i] = b[j];
+}
+}
+}
 
-  return (s);
+return (s);
 }
