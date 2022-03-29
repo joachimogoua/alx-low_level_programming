3f2b7e04ec955fb02da53d0cf16a386d7801eefa
joachimogoua <ogouadjedjijoachim@gmail.com>
2022-03-29 13:27:16 -0700

    3.strpn

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -12,5 +12,6 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 
 #endif
