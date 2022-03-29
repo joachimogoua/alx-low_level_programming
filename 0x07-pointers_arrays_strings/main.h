b9e143723f1f13e836cda8493c0777bed7e7b2a4
joachimogoua <ogouadjedjijoachim@gmail.com>
2022-03-29 13:40:13 -0700

    4.strpbrk

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -13,5 +13,6 @@ void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
 
 #endif
