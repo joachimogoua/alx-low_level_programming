ef605caef4b303ec4d6ab83c8d225f3e01f48491
joachimogoua <ogouadjedjijoachim@gmail.com>
2022-03-31 12:59:59 -0700

    4.pow recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,19 +6,18 @@
 * @x:fisrt parameter
 */
 
-
 int _pow_recursion(int x, int y)
 {
 if (y < 0)
 {
 return (-1);
 }
-if else (y == 0)
+else if (y == 0)
 {
 return (1);
 }
 else
 {
-  return (x * _pow_recursion(x, y-1));
+return (x * _pow_recursion(x, y-1));
 }
 }
